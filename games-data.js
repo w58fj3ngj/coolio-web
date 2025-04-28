e389c78be29907a3c11cda64bd3bc5ecf7928f30
Coolio <156616687+w58fj3ngj@users.noreply.github.com>
2025-04-28 18:26:10 -0500

    john

---
+++ games-data.js
@@ -0,0 +1,582 @@
+const games = [
+  {
+    "name": "1v1.lol",
+    "image": "games/1v1lol.webp",
+    "url": "games/1v1-lol/index.html"
+  },
+  {
+    "name": "2048",
+    "image": "games/2048.webp",
+    "url": "games/2048/index.html"
+  },
+  {
+    "name": "3D Bowling",
+    "url": "games/3d-bowling/index.html",
+    "image": "games/3dbowling.webp"
+  },
+  {
+    "name": "8 Ball Pool",
+    "url": "games/8ballpool/index.html",
+    "image": "games/8ballpool.webp"
+  },
+  {
+    "name": "Awesome Tanks 2",
+    "image": "games/awesometanks2.webp",
+    "url": "games/awesome-tanks-2/index.html"
+  },
+  {
+    "name": "Bacon May Die",
+    "url": "games/baconmaydie/index.html",
+    "image": "games/baconmaydie.webp"
+  },
+  {
+    "name": "Bad Ice-Cream",
+    "image": "games/badicecream.webp",
+    "url": "games/bad-icecream/index.html"
+  },
+  {
+    "name": "Baldis Basics",
+    "image": "games/baldisbasics.webp",
+    "url": "games/baldis-basics/index.html"
+  },
+  {
+    "name": "Basket Random",
+    "url": "games/basket-random/index.html",
+    "image": "games/basketrandom.webp"
+  },
+  {
+    "name": "Basketball Stars",
+    "url": "games/basketball-stars/index.html",
+    "image": "games/basketballstars.webp"
+  },
+  {
+    "name": "Bitlife",
+    "image": "games/bitlife.webp",
+    "url": "games/bit-life/index.html"
+  },
+  {
+    "name": "Block Blast",
+    "url": "games/blockblast/index.html",
+    "image": "games/blockblast.webp"
+  },
+  {
+    "name": "Blocky Snakes",
+    "url": "games/blocky-snakes/index.html",
+    "image": "games/blockysnakes.webp"
+  },
+  {
+    "name": "Bloxorz",
+    "image": "games/bloxorz.webp",
+    "url": "games/bloxorz/index.html"
+  },
+  {
+    "name": "Bottle Flip 3D",
+    "url": "games/bottle-flip-3d/index.html",
+    "image": "games/bottleflip3d.webp"
+  },
+  {
+    "name": "Boxing Random",
+    "url": "games/boxing-random/index.html",
+    "image": "games/boxingrandom.webp"
+  },
+  {
+    "name": "Burrito Bison",
+    "url": "games/burrito-bison/index.html",
+    "image": "games/burritobison.webp"
+  },
+  {
+    "name": "Candy Jump",
+    "url": "games/candyjump/index.html",
+    "image": "games/candyjump.webp"
+  },
+  {
+    "name": "Chrome Dino",
+    "image": "games/chromedino.webp",
+    "url": "games/chrome-dino/index.html"
+  },
+  {
+    "name": "Clicker Heroes",
+    "image": "games/clickerheros.webp",
+    "url": "games/clicker-heros/index.html"
+  },
+  {
+    "name": "Conway's Game of Life",
+    "image": "games/conwaysgameoflife.webp",
+    "url": "games/conways-game-of-life/index.html"
+  },
+  {
+    "name": "Cookie Clicker",
+    "image": "games/cookieclicker.webp",
+    "url": "games/cookie-clicker/index.html"
+  },
+  {
+    "name": "Crazy Cars",
+    "image": "games/crazycars.webp",
+    "url": "games/crazy-cars/index.html"
+  },
+  {
+    "name": "Crazy Cattle 3D",
+    "url": "games/crazy-cattle-3d/index.html",
+    "image": "games/crazycattle3d.webp"
+  },
+  {
+    "name": "Crossy Road",
+    "image": "games/crossyroad.webp",
+    "url": "games/crossy-road/index.html"
+  },
+  {
+    "name": "Crossy Road Space",
+    "image": "games/crossyroadspace.webp",
+    "url": "games/crossy-road-space/index.html"
+  },
+  {
+    "name": "Cubes 2048",
+    "image": "games/cubes2048.webp",
+    "url": "games/cubes-2048/index.html"
+  },
+  {
+    "name": "Dead Again",
+    "url": "games/dead-again/index.html",
+    "image": "games/deadagain.webp"
+  },
+  {
+    "name": "Death Run 3D",
+    "url": "games/death-run-3d/index.html",
+    "image": "games/deathrun3d.webp"
+  },
+  {
+    "name": "Doge Miner",
+    "image": "games/dogeminer.webp",
+    "url": "games/doge-miner/index.html"
+  },
+  {
+    "name": "Doodle Cricket",
+    "url": "games/doodle-cricket/index.html",
+    "image": "games/doodlecricket.webp"
+  },
+  {
+    "name": "Down The Hill",
+    "url": "games/downthehill/index.html",
+    "image": "games/downthehill.webp"
+  },
+  {
+    "name": "Drift Boss",
+    "image": "games/driftboss.webp",
+    "url": "games/drift-boss/index.html"
+  },
+  {
+    "name": "Drift Hunters",
+    "image": "games/drifthunters.webp",
+    "url": "games/drift-hunters/index.html"
+  },
+  {
+    "name": "Drive Mad",
+    "url": "games/drive-mad/index.html",
+    "image": "games/drivemad.webp"
+  },
+  {
+    "name": "Duck Life",
+    "image": "games/ducklife1.webp",
+    "url": "games/duck-life-1/index.html"
+  },
+  {
+    "name": "Duck Life 2",
+    "image": "games/ducklife2.webp",
+    "url": "games/duck-life-2/index.html"
+  },
+  {
+    "name": "Duck Life 3",
+    "image": "games/ducklife3.webp",
+    "url": "games/duck-life-3/index.html"
+  },
+  {
+    "name": "Duck Life 4",
+    "image": "games/ducklife4.webp",
+    "url": "games/duck-life-4/index.html"
+  },
+  {
+    "name": "Eaglercraft 1.5.2",
+    "url": "games/eaglercraft/index.html",
+    "image": "games/eaglercraft.webp"
+  },
+  {
+    "name": "Earn To Die",
+    "url": "games/earn-to-die/index.html",
+    "image": "games/earntodie.webp"
+  },
+  {
+    "name": "Eggy Car",
+    "url": "games/eggycar/index.html",
+    "image": "games/eggycar.webp"
+  },
+  {
+    "name": "Elastic Man",
+    "image": "games/elasticman.webp",
+    "url": "games/elasticman/index.html"
+  },
+  {
+    "name": "Fruit Merge",
+    "url": "games/fruitmerge/index.html",
+    "image": "games/fruitmerge.webp"
+  },
+  {
+    "name": "Golf Champions",
+    "url": "games/golfchampions/index.html",
+    "image": "games/golfchampions.webp"
+  },
+  {
+    "name": "Grindcraft",
+    "url": "games/grindcraft/index.html",
+    "image": "games/grindcraft.webp"
+  },
+  {
+    "name": "Gunspin",
+    "url": "games/gunspin/index.html",
+    "image": "games/gunspin.webp"
+  },
+  {
+    "name": "Gunspin",
+    "image": "games/gunspin.webp",
+    "url": "games/gunspin/index.html"
+  },
+  {
+    "name": "Hide And Smash",
+    "url": "games/hide-and-smash/index.html",
+    "image": "games/hideandsmash.webp"
+  },
+  {
+    "name": "Hole.io",
+    "url": "games/hole-io/index.html",
+    "image": "games/holeio.webp"
+  },
+  {
+    "name": "Idle Ants",
+    "image": "games/idleants.webp",
+    "url": "games/idle-ants/index.html"
+  },
+  {
+    "name": "Idle Breakout",
+    "image": "games/idlebreakout.webp",
+    "url": "games/idle-breakout/index.html"
+  },
+  {
+    "name": "Idle Tree City",
+    "url": "games/idle-tree-city/index.html",
+    "image": "games/idletreecity.webp"
+  },
+  {
+    "name": "Jetpack Joyride",
+    "url": "games/jetpackjoyride/index.html",
+    "image": "games/jetpackjoyride.webp"
+  },
+  {
+    "name": "Johnny Upgrade",
+    "image": "games/johnnyupgrade.webp",
+    "url": "games/johnnyupgrade/index.html"
+  },
+  {
+    "name": "Jungle Tower Defense",
+    "url": "games/jungletowerdefense/index.html",
+    "image": "games/jungletowerdefense.webp"
+  },
+  {
+    "name": "Just Fall Lol",
+    "url": "games/just-fall-lol.html",
+    "image": "games/justfalllol.webp"
+  },
+  {
+    "name": "Leader Strike",
+    "url": "games/leader-strike/index.html",
+    "image": "games/leaderstrike.webp"
+  },
+  {
+    "name": "Learn to Fly",
+    "image": "games/learntofly.webp",
+    "url": "games/learn-to-fly-1/index.html"
+  },
+  {
+    "name": "Masked Forces",
+    "url": "games/maskedforces/index.html",
+    "image": "games/maskedforces.webp"
+  },
+  {
+    "name": "Minecraft Classic",
+    "url": "games/minecraft-classic/index.html",
+    "image": "games/minecraftclassic.webp"
+  },
+  {
+    "name": "Monkey Mart",
+    "image": "games/monkeymart.webp",
+    "url": "games/monkey-mart/index.html"
+  },
+  {
+    "name": "Monster Tracks",
+    "url": "games/monstertracks/index.html",
+    "image": "games/monstertracks.webp"
+  },
+  {
+    "name": "Moto X3M",
+    "url": "games/moto-x3m/index.html",
+    "image": "games/motox3m.webp"
+  },
+  {
+    "name": "Moto X3M 2",
+    "url": "games/moto-x3m-2/index.html",
+    "image": "games/motox3m2.webp"
+  },
+  {
+    "name": "Moto X3M Pool Party",
+    "url": "games/moto-x3m-pool-party/index.html",
+    "image": "games/motox3mpoolparty.webp"
+  },
+  {
+    "name": "Moto X3M Spookyland",
+    "url": "games/moto-x3m-spookyland/index.html",
+    "image": "games/motox3mspookyland.webp"
+  },
+  {
+    "name": "Moto X3M Winter",
+    "url": "games/moto-x3m-winter/index.html",
+    "image": "games/motox3mwinter.webp"
+  },
+  {
+    "name": "Papa's Burgeria",
+    "url": "games/papas-burgeria/index.html",
+    "image": "games/papasburgeria.webp"
+  },
+  {
+    "name": "Papa's Freezeria",
+    "url": "games/papas-freezeria/index.html",
+    "image": "games/papasfreezeria.webp"
+  },
+  {
+    "name": "Papa's Pizzaria",
+    "url": "games/papas-pizzaria/index.html",
+    "image": "games/papaspizzaria.webp"
+  },
+  {
+    "name": "Papa's Sushiria",
+    "url": "games/papas-sushiria/index.html",
+    "image": "games/papassushiria.webp"
+  },
+  {
+    "name": "Paper.io 2",
+    "image": "games/paperio2.webp",
+    "url": "games/paperio2/index.html"
+  },
+  {
+    "name": "Penalty Kick Online",
+    "url": "games/penalty-kick-online/index.html",
+    "image": "games/penaltykickonline.webp"
+  },
+  {
+    "name": "Power Badminton",
+    "url": "games/pbm/index.html",
+    "image": "games/pbm.webp"
+  },
+  {
+    "name": "President Simulator",
+    "image": "games/presidentsimulator.webp",
+    "url": "games/presidentsimulator/index.html"
+  },
+  {
+    "name": "Push The Box",
+    "url": "games/push-the-box/index.html",
+    "image": "games/pushthebox.webp"
+  },
+  {
+    "name": "Recoil",
+    "url": "games/recoil/index.html",
+    "image": "games/recoil.webp"
+  },
+  {
+    "name": "Retro Bowl",
+    "image": "games/retrobowl.webp",
+    "url": "games/retro-bowl/index.html"
+  },
+  {
+    "name": "Retro Bowl College",
+    "image": "games/retrobowlcollege.webp",
+    "url": "games/retro-bowl-college/index.html"
+  },
+  {
+    "name": "Retro Highway",
+    "url": "games/retro-highway/index.html",
+    "image": "games/retrohighway.webp"
+  },
+  {
+    "name": "Rooftop Snipers",
+    "image": "games/rooftopsnipers.webp",
+    "url": "games/rooftop-snipers/index.html"
+  },
+  {
+    "name": "Sausage Flip",
+    "url": "games/sausageflip/index.html",
+    "image": "games/sausageflip.webp"
+  },
+  {
+    "name": "Shell Shockers",
+    "image": "games/shellshockers.webp",
+    "url": "games/shell-shockers/index.html"
+  },
+  {
+    "name": "Skillfite.io",
+    "url": "games/skillfite-io/index.html",
+    "image": "games/skillfiteio.webp"
+  },
+  {
+    "name": "Sling Drift",
+    "url": "games/sling-drift/index.html",
+    "image": "games/slingdrift.webp"
+  },
+  {
+    "name": "Slope",
+    "url": "games/slope/index.html",
+    "image": "games/slope.webp"
+  },
+  {
+    "name": "Slope 2 Player",
+    "url": "games/slope-2-player/index.html",
+    "image": "games/slope2player.webp"
+  },
+  {
+    "name": "Sniper Shot",
+    "url": "games/snipershot/index.html",
+    "image": "games/snipershot.webp"
+  },
+  {
+    "name": "Snow Rider 3D",
+    "image": "games/snowrider3d.webp",
+    "url": "games/snow-rider-3d/index.html"
+  },
+  {
+    "name": "Soccer Random",
+    "url": "games/soccer-random/index.html",
+    "image": "games/soccerrandom.webp"
+  },
+  {
+    "name": "Stack",
+    "url": "games/stack/index.html",
+    "image": "games/stack.webp"
+  },
+  {
+    "name": "Stickman Hook",
+    "url": "games/stickman-hook/index.html",
+    "image": "games/stickmanhook.webp"
+  },
+  {
+    "name": "Stickman Ragdoll",
+    "image": "games/stickmanragdoll.webp",
+    "url": "games/stickmanragdoll/index.html"
+  },
+  {
+    "name": "Streetball Star",
+    "url": "static/iframe.html#https://streetballstar.games235.com/",
+    "image": "games/streetballstar.webp"
+  },
+  {
+    "name": "Subway Surfers",
+    "image": "games/subwaysurfers.webp",
+    "url": "games/subway-surfers/index.html"
+  },
+  {
+    "name": "Super Mario 64",
+    "url": "games/super-mario-64/index.html",
+    "image": "games/supermario64.webp"
+  },
+  {
+    "name": "Super Mario Bros",
+    "url": "games/super-mario-bros/index.html",
+    "image": "games/supermariobros.webp"
+  },
+  {
+    "name": "Super Star Car",
+    "url": "static/iframe.html#https://class811.github.io/g3/super-star-car/",
+    "image": "games/superstarcar.webp"
+  },
+  {
+    "name": "Temple Of Boom",
+    "url": "games/temple-of-boom/index.html",
+    "image": "games/templeofboom.webp"
+  },
+  {
+    "name": "Temple Run 2",
+    "url": "games/temple-run-2/index.html",
+    "image": "games/templerun2.webp"
+  },
+  {
+    "name": "The Impossible Quiz",
+    "url": "games/the-impossible-quiz/index.html",
+    "image": "games/theimpossiblequiz.webp"
+  },
+  {
+    "name": "The Impossible Quiz 2",
+    "image": "games/tiq2.webp",
+    "url": "games/tiq2/index.html"
+  },
+  {
+    "name": "Time Shooter 3",
+    "image": "games/timeshooter3.webp",
+    "url": "games/time-shooter-3/index.html"
+  },
+  {
+    "name": "Tiny Fishing",
+    "image": "games/tinyfishing.webp",
+    "url": "games/tiny-fishing/index.html"
+  },
+  {
+    "name": "Tomb of the Mask",
+    "image": "games/tombofthemask.webp",
+    "url": "games/tomb-of-the-mask/index.html"
+  },
+  {
+    "name": "Traffic Rush",
+    "image": "games/trafficrush.webp",
+    "url": "games/traffic-rush/index.html"
+  },
+  {
+    "name": "Trafficmania",
+    "url": "games/trafficmania/index.html",
+    "image": "games/trafficmania.webp"
+  },
+  {
+    "name": "Tricks",
+    "url": "games/tricks/index.html",
+    "image": "games/tricks.webp"
+  },
+  {
+    "name": "Tube Clicker",
+    "image": "games/tubeclicker.webp",
+    "url": "games/tube-clicker/index.html"
+  },
+  {
+    "name": "Tunnel Rush",
+    "url": "games/tunnel-rush/index.html",
+    "image": "games/tunnelrush.webp"
+  },
+  {
+    "name": "Tunnel Rush 2",
+    "url": "games/tunnel-rush-2/index.html",
+    "image": "games/tunnelrush2.webp"
+  },
+  {
+    "name": "Uno 4 Colors",
+    "url": "games/uno-4-colors/index.html",
+    "image": "games/uno4colors.webp"
+  },
+  {
+    "name": "Volley Random",
+    "url": "games/volley-random/index.html",
+    "image": "games/volleyrandom.webp"
+  },
+  {
+    "name": "Wordle+",
+    "url": "games/wordle-plus/index.html",
+    "image": "games/wordleplus.webp"
+  },
+  {
+    "name": "Zombie Derby Pixel Survival",
+    "url": "games/zombie-derby-pixel-survival/index.html",
+    "image": "games/zombiederbypixelsurvival.webp"
+  }
+];
