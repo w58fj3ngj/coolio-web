b1d300edabaa8f907bf9d66b95db43a0ab8464ec
Coolio <156616687+w58fj3ngj@users.noreply.github.com>
2025-09-20 15:25:44 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: "https://bare-sigma.yourmom.eu.org/",
+    bare: "https://i-thought-i-could-trust-you-no-diddy-dog.satucat.uk/tspmo/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
